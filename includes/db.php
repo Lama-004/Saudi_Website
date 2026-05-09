d6c75d9d10d39f6d47341994b0a78f4ff1a6dc32
Lama-004 <161324856+Lama-004@users.noreply.github.com>
2026-05-09 16:40:49 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,13 @@
 <?php
-
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'saudi_website');
+define('DB_PASS', 'pXVPCEJAgSSOpvssNYCdHRafOownmhdi');
+define('DB_NAME', 'railway');
+define('DB_PORT', 3306);
 
 function getConnection() {
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
     $conn->set_charset("utf8mb4");
-    
     if ($conn->connect_error) {
         die("خطأ في الاتصال بقاعدة البيانات: " . $conn->connect_error);
     }
@@ -16,5 +15,4 @@ function getConnection() {
 }
 
 $conn = getConnection();
-
 ?>
